8f8f48c7a3f763f789c2b27b93f41908c6b2ab25
junk1073 <junk1073@mail.ru>
2026-02-07 14:15:39 +0300

    feat: create ToastContext skeleton

--- src/context/ToastContext.tsx
+++ src/context/ToastContext.tsx
@@ -1,25 +1,43 @@
-import React, { useState, type ReactNode } from 'react';
+import React, {
+  createContext,
+  useContext,
+  useState,
+  type ReactNode,
+} from 'react';
 import type { Toast } from '../types/types';
 
+type ToastContextValue = {
+  toasts: Toast[];
+  addToast: (toast: Omit<Toast, 'id'>) => void;
+  removeToast: (id: string) => void;
+};
+
+const ToastContext = createContext<ToastContextValue | null>(null);
 
 export const ToastProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
   const [toasts, setToasts] = useState<Toast[]>([]);
 
   const addToast = (toast: Omit<Toast, 'id'>) => {
-    
+    // позже напишем
   };
 
   const removeToast = (id: string) => {
-    
+    // позже напишем
   };
 
   return (
-   <>
-
-   </>
+    <ToastContext.Provider value={{ toasts, addToast, removeToast }}>
+      {children}
+    </ToastContext.Provider>
   );
 };
 
 export const useToast = () => {
-  
+  const context = useContext(ToastContext);
+
+  if (!context) {
+    throw new Error('useToast must be used within ToastProvider');
+  }
+
+  return context;
 };
